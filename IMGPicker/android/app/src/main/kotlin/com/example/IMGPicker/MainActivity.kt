fba9a4acfbd5b2824fb9936c5be6df57c32521c6
Anna-12-Mer <mammeramna@gmail.Com>
2021-06-01 09:36:44 +0200

    PICK FROM GALLERY

---
+++ IMGPicker/android/app/src/main/kotlin/com/example/IMGPicker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.IMGPicker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ IMGPicker/lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File imageFile;
+
+  //** Get from gallery */
+  _getFromGallery() async {
+    PickedFile pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IMGPicker'),
+      ),
+      body: Center(
+        child: imageFile == null
+            ? FlatButton(
+                textColor: Color(0xf05542), //
+                onPressed: () {
+                  _getFromGallery();
+                },
+                child: Text('PICK FROM GALLERY'),
+              )
+            : Container(
+                child: Image.file(
+                  imageFile,
+                  fit: BoxFit.cover,
+                ),
+              ),
+      ),
+    );
+  }
+}
